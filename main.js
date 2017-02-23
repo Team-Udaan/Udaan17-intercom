70731f20b42d325f10f51dcb5d33292f26335a53
maunilswadas <swadasmaunil@gmail.com>
2017-02-24 02:20:22 +0530

    update: removed materialized and added custom modals

---
+++ main.js
@@ -0,0 +1,116 @@
+/**
+ * Created by maunil on 23-02-2017.
+ */
+
+var allPeers = [];
+var peerInfo = {};
+var webrtc = null;
+var onCall = false;
+var currentPeer = null;
+var callingTimeout = null;
+
+function findPeer(id) {
+    return allPeers.find(function (obj) {
+        return obj.id == id;
+    });
+}
+function openModal(id) {
+    var modal = document.getElementById(id);
+    modal.style.display = "block";
+}
+function closeModal(id) {
+    var modal = document.getElementById(id);
+    modal.style.display = "none";
+}
+
+$('#submit').click(function () {
+    var username = $('#username').val();
+    peerInfo.username = username;
+    webrtc = new SimpleWebRTC({
+        url: 'https://' + location.hostname + ":8888",
+        autoRequestMedia: false,
+        nick: username,
+        options: {video: false, audio: false}
+    });
+    webrtc.joinRoom('idle');
+    webrtc.on('createdPeer', function (peer) {
+        setTimeout(function () {
+            updatePeerList();
+        }, 1000);
+    });
+    webrtc.connection.on('remove', function () {
+        updatePeerList();
+    });
+    $('#cancel').click(function () {
+        currentPeer.send('canceled', 'canceled');
+        closeModal('calling-modal');
+        onCall = false;
+    });
+
+    function updatePeerList() {
+        allPeers = webrtc.webrtc.peers;
+        var peerListTemplate = Handlebars.templates['calling-buttons']({'allPeers': allPeers});
+        $('#peers').html(peerListTemplate);
+        $('.call').click(function () {
+            var id = $(this).data('id');
+            currentPeer = allPeers.find(function (obj) {
+                return obj.id == id;
+            });
+            onCall = true;
+            currentPeer.send('calling', {
+                'sender': webrtc.connection.connection.id
+            });
+            openModal('calling-modal');
+            setTimeout(function () {
+                onCall = false;
+                closeModal('calling-modal');
+            }, 10000);
+        });
+    }
+
+    webrtc.connection.on('message', function (data) {
+        if (data.type == 'calling') {
+            if (onCall) {
+                peer = findPeer(data.payload.sender);
+                peer.send('rejected', 'busy');
+            }
+            else {
+                onCall = true;
+                openModal('receiving-modal');
+                setTimeout(function () {
+                    closeModal('receiving-modal');
+                    onCall = false;
+                }, 10000);
+                $('#accept').click(function () {
+                    peer = findPeer(data.payload.sender);
+                    peer.send('accepted', data.payload.sender);
+                    page('/activePage.html?' + data.payload.sender);
+                });
+                $('#reject').click(function () {
+                    closeModal('receiving-modal');
+                    peer = findPeer(data.payload.sender);
+                    peer.send('rejected', 'rejected');
+                    onCall = false;
+                });
+            }
+        }
+        if (data.type == 'accepted') {
+            page('/activePage.html?' + data.payload);
+        }
+        if (data.type == 'rejected') {
+            var modalHeading = $('#calling-modal .modal-content h4');
+
+            modalHeading.html('Rejected');
+            clearTimeout(callingTimeout);
+            setTimeout(function () {
+                closeModal('calling-modal');
+                modalHeading.html('Calling');
+            }, 3000);
+            onCall = false;
+        }
+        if (data.type == 'canceled') {
+            closeModal('receiving-modal');
+            onCall = false;
+        }
+    });
+});
